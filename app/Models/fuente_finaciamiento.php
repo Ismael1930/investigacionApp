cbfd233b252a3947d01fa17eb0ddc8f40d953aca
Ismael <ismaelfelizestudios@gmail.com>
2024-05-21 11:26:52 -0400

    creando modelos

---
+++ app/Models/fuente_finaciamiento.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class fuente_finaciamiento extends Model
+{
+    use HasFactory;
+}
